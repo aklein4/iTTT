3eb034cc951762553d4e8747e183e2db0e067405
Adam Klein <adam3207@gmail.com>
2026-02-15 22:43:52 +0000

    plot new data

--- src/plot_data.py
+++ src/plot_data.py
@@ -3,28 +3,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-FILE = "new_loss.csv"
+FILE = "loss.csv"
 
 RUNS = [
     "longattn-seperated",
-    "longattn",
-    "longattn-aux",
+    "longattn-no-muon",
+    # "longattn-aux-v2",
+    "longattn-fo-grad"
 ]
+COLORS = ["black", "red", "blue"]
 
 
 def main():
     
     df = pd.read_csv(FILE)
 
-    for run in RUNS:
+    a = df[f"{RUNS[0]} - loss"][:500].rolling(window=200).mean()
+    b = df[f"{RUNS[-1]} - loss"][:500].rolling(window=200).mean()
+    plt.plot(b - a)
+    plt.grid()
+    plt.savefig("diff.png")
+    return
+
+    for i, run in enumerate(RUNS):
         
         # dec_0 = df[f"{run} - grouped_lm_loss/decade_0"]
         # dec_6 = df[f"{run} - grouped_lm_loss/decade_6"]
         # x = dec_6 / dec_0
-        x = df[f"{run} - loss"]
+        x = df[f"{run} - loss"][:800]
 
-        x_running = x.rolling(window=100)
-        plt.plot(x_running.mean(), label=run)
+        x_running = x.rolling(window=200)
+        plt.plot(x_running.mean(), label=run, color=COLORS[i])
 
     plt.legend()
     plt.grid()
